1fd4340f650f2897accb2b0764c2907e37f473a5
Isaac G <shxzzza@gmail.com>
2021-07-06 18:50:07 +1000

    About mostly organised

--- src/components/About.js
+++ src/components/About.js
@@ -5,59 +5,57 @@ import CircleAccent from '../assets/icons/CircleAccent';
 import theme from '../assets/styles/theme';
 import AboutImage from '../assets/images/AboutPicture.png';
 
-// Work out if you need this part ...
-const AboutFrame = styled.div`
-
-	.section-rectangle {
-		height: 20px;
-		width: 100px;
-		background: ${theme.colours.lightBlue};
-	}
-	.y-rectangle {
-		height: 200px;
-		width: 2px;
-		background: ${theme.colours.grey};
-	}
-`;
-
 const AboutSection = styled.section`
 	background: linear-gradient(90deg, ${theme.colours.darkGrey} 70%, ${theme.colours.white} 30%);
+	display: grid;
+    grid-template-columns: 60vw 40vw;
 
 	.section-rectangle {
-		height: 20px;
-		width: 100px;
+		height: 2vh;
+		width: 8vw;
 		background: ${theme.colours.lightBlue};
-	}
-	.y-rectangle {
-		height: 200px;
-		width: 2px;
-		background: ${theme.colours.grey};
 	}
 
 	.about-left-container {
-		// setup
-		margin-left; 40px;
+		margin-left: 10vw;
+		margin-right: 10vw;
+		height: 100vh;
 
-		.section-num {
+		ul {
 			font-size: 48px;
 			font-family: 'Space Mono', sans-serif;
 			color: ${theme.colours.grey};
+			padding: 0;
+			margin: 0;
+			list-style: none;
+			text-decoration: none;
+			
+			overflow: hidden;
+
+			li {
+				float: left;
+				display: flex;
+				padding: 1vh;
+				text-decoration: none;
+
+				div {
+					height: 2px;
+					width: 50vw;
+					background: ${theme.colours.grey};
+				}
+			}
 		}
 
-		.x-rectangle {
-			height: 2px;
-			width: 400px;
-			background: ${theme.colours.grey};
-		}
-		
 		.about-text {
-			font-size: 24px;
+			padding-top: 10vh;
+			font-size: 26px;
 			font-family: 'Space Mono', sans-serif;
 			color: ${theme.colours.lightGrey};
+			text-align: left;
 		}
 
 		.about-learning {
-			font-size: 24px;
+			font-size: 26px;
 			font-family: 'Space Mono', sans-serif;
 			color: ${theme.colours.lightGrey};
 
@@ -68,9 +66,10 @@ const AboutSection = styled.section`
 	}
 
 	.about-right-container {
-		
+		margin-left: 2vw;
+		margin-right: 10vw;
 		h1 {
-			font-size: 144px;
+			font-size: 120px;
 			color: ${theme.colours.white};
 			span {
 				color: ${theme.colours.lightBlue};
@@ -85,28 +84,36 @@ const About = () => {
 	return (
 		<>
 		<AboutSection>
-		<div className="section-rectangle"/>
-			<div className="y-rectangle"/>
-			<div className="about-left-container">
-				<p className="section-num">01</p>
-				<div className="x-rectangle"/>
-				<p className="about-text">
-					Hi, I’m Isaac.<br/>
-					I am an Australian developer currently living in Wollongong with a B.E & B.CS. I enjoy the design and development process in a range of topics such as computer vision, web-development, security & hardware projects.
-					<br/>
-					I have experience creating things in teams as well as many solo-projects, all of which you can read about on this site!
-					<br/>
-					Other than tech, I enjoy reading, rock-climbing & making music. 
-				</p>
-				<p class="about-learning">
-					Currently I am learning: <span>Vue</span> |
-				</p>
-			</div>
-			<div className="about-right-container">
-				<h1>about <span>me.</span></h1>
-				<img src={AboutImage} alt="Picture of Isaac G"></img>
-				<CircleAccent/>
-			</div>
+				<div className="section-rectangle"/>
+				<div className="y-rectangle"/>
+				<div className="about-left-container">
+					<ul>
+						<li><p>01</p></li>
+						<li><div/></li>
+					</ul>
+					<p className="about-text">
+						Hi, I’m Isaac.
+						<br/>
+						<br/>
+						I am an Australian developer currently living in Wollongong with a B.E & B.CS. I enjoy the design and development process in a range of topics such as computer vision, web-development, security & hardware projects.
+						<br/>
+						<br/>
+						I have experience creating things in teams as well as many solo-projects, all of which you can read about on this site!
+						<br/>
+						<br/>
+						Other than tech, I enjoy reading, rock-climbing & making music. 
+					</p>
+					<p class="about-learning">
+						<br/>
+						<br/>
+						Currently I am learning: <span>Vue</span>|
+					</p>
+				</div>
+				<div className="about-right-container">
+					<h1>about <br/><span>me.</span></h1>
+					<img src={AboutImage} alt="Picture of Isaac G"></img>
+					<CircleAccent/>
+				</div>
 		</AboutSection>
 		</>
 	);
